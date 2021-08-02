35fedffb71af0ce2f78e7cb9a0f4e9743e8dde83
piyush0810 <piyushkumar0810@gmail.com>
2021-08-03 01:11:47 +0530

    test

---
+++ Controllers/CityController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using poc.Data;
+using poc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace poc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase {
+        private readonly DataContext dc;
+
+        public CityController(DataContext dc) {
+            this.dc = dc;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCities()
+        {
+            var cities = dc.Cities.ToList();
+
+            return Ok(cities);
+            
+        }
+
+        [HttpPost("ADD/{cityname}")]
+        public async Task<IActionResult> AddCity(string cityname)
+        {
+            City city = new();
+            city.Name = cityname;
+            await dc.Cities.AddAsync(city);
+            await dc.SaveChangesAsync();
+            return Ok(city);
+
+            
+        }
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            if (id > 1)
+            {
+                return "value";
+            }
+            return "no value";
+        }
+        }
+
+}
+
+
---
+++ Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using poc.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace poc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers();
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("default")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "poc", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "poc v1"));
+            }
+
+            app.UseRouting();
+            app.UseCors(m => m.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
